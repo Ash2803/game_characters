3c11c06c2a2bfd4184872001d1309d981c11d43b
Ashot Abazyan <ashot.93@mail.ru>
2022-06-18 00:56:57 +0300

    18/06/22 v1

---
+++ char_create.py
@@ -0,0 +1,61 @@
+import file_operations
+from faker import Faker
+import random
+
+
+def main():
+    fake = Faker("ru_RU")
+    skills = ["Дипломатия", "Убеждение", "Изготовление ядов", "Обман", "Хитрость", "Прямолинейность", "Харизма",
+              "Креативность", "Продуктивность", "Критическое мышление", "Инициативность", "Гибкость"]
+    rune_dict = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    runic_skills = []
+    for skill in skills:
+        runic_skills.append('')
+        for letter in skill:
+            runic_skills[-1] += rune_dict.get(letter, "")
+
+    for i in range(0, 10):
+        sorted_skills = random.sample(runic_skills, 3)
+        card = {
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "job": fake.job(),
+            "town": fake.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": sorted_skills[0],
+            "skill_2": sorted_skills[1],
+            "skill_3": sorted_skills[2]
+        }
+        file_operations.render_template(f"src/charsheet.svg", f"output/svg/charsheet-{i}.svg", card)
+
+
+if __name__ == '__main__':
+    main()
